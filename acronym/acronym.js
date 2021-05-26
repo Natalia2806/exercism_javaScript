1b6c3c3e10193cf102aee796d49584a87b05af5c
Natalia2806 <tuirannatalia@gmail.com>
2021-05-26 01:33:37 -0500

    Cambios en el ejercicio acronym

--- acronym/acronym.js
+++ acronym/acronym.js
@@ -1,21 +1,9 @@
-//
-// This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const parse = (string) => {
+  let acronym = string
+    .toUpperCase()
+    .replace(/[_']/, "")
+    .match(/\b[a-z]/gi)
+    .join("");
 
-export const parse = (str) => {
-  let arr = str
-     .replace(/[_-]/g, " ")
-     .replace(/[']/g, "")
-     .replace(/ {1,}/g, " ");
-
-   return arr
-     .split(" ")
-     .map((a) => a[0].toUpperCase())
-     .join("");
+  return acronym;
 };
-
-
-
-
-
